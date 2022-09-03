5c3f82dddc35d462eafa756f0c82353b56671b0f
HebrinJ <ashram28@gmail.com>
2022-09-03 17:47:43 +0400

    Создана базовая модель данных

---
+++ Model/ToDoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Model
+{
+    class ToDoModel
+    {
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        private bool isDone;
+        private string text;
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set { isDone = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+    }
+}
